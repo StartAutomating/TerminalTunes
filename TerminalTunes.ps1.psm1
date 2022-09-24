34261d471031f92e9ea26f6056bb781191e7a823
James Brundage <@github.com>
2022-09-24 14:02:48 -0700

    Adding TerminalTunes module (Fixes #4)

---
+++ TerminalTunes.ps1.psm1
@@ -0,0 +1,22 @@
+﻿[Include('*-*')]$psScriptRoot
+
+$Script:TuneShortTitleToLongTitle = [Ordered]@{}
+$script:TuneLongtitleToShortTitle = [Ordered]@{}
+Get-Tune | 
+    ForEach-Object {
+        $shortTitle = $_.Title -replace '\W'
+        $script:TuneLongtitleToShortTitle[$_.Title] = $shortTitle
+        $Script:TuneShortTitleToLongTitle[$shortTitle] = $_.Title
+        if ($_.IsGenerator) {
+            $tuneGenerator = $_
+            foreach ($tuneAlias in $tuneGenerator.ScriptBlock.Attributes.AliasNames) {
+                if ($tuneAlias -and $tuneAlias -ne $shortTitle) { 
+                    $Script:TuneShortTitleToLongTitle[$tuneAlias] = $_.Title
+                    Set-Alias "🎶$($tuneAlias)🎶" Start-Tune
+                }    
+            }
+        }
+        Set-Alias "🎶$($shortTitle)🎶" Start-Tune
+    }
+
+Export-ModuleMember -Function * -Alias *
---
+++ TerminalTunes.psd1
@@ -0,0 +1,17 @@
+@{
+    ModuleVersion    = '0.1'
+    RootModule       = 'TerminalTunes.psm1'
+    Description      = 'A Music Player for Terminals'
+    TypesToProcess   = 'TerminalTunes.types.ps1xml'
+    FormatsToProcess = 'TerminalTunes.format.ps1xml'
+    Guid             = '66e51179-3815-4557-b8e9-63309326c45d'
+    Author           = 'James Brundage'
+    CompanyName      = 'Start-Automating'
+    Copyright        = '2022'
+    PrivateData      = @{
+        PSData = @{
+            ProjectURI = 'https://github.com/StartAutomating/TerminalTunes'
+            LicenseURI = 'https://github.com/StartAutomating/TerminalTunes/blob/main/LICENSE'
+        }
+    }
+}
