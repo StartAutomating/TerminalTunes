a7b4063bfa301f9750fa82184ad9e659ff27f09e
StartAutomating <james.brundage@start-automating.com>
2025-11-18 21:09:19 -0800

    fix: Moving Build to /Build and updating workflow ( Fixes #35 )
    Updating workflow definition

--- Build/TerminalTunes.GitHubWorkflow.PSDevOps.ps1
+++ Build/TerminalTunes.GitHubWorkflow.PSDevOps.ps1
@@ -5,7 +5,7 @@ Import-BuildStep -SourcePath (
 ) -BuildSystem GitHubWorkflow
 
 Push-Location ($PSScriptRoot | Split-Path)
-New-GitHubWorkflow -Name "Build Turtle Module" -On Push,
+New-GitHubWorkflow -Name "Build Module" -On Push,
     PullRequest, 
     Demand -Job  TestPowerShellOnLinux, 
     TagReleaseAndPublish, BuildModule -OutputPath .\.github\workflows\TestAndPublish.yml
@@ -13,6 +13,4 @@ New-GitHubWorkflow -Name "Build Turtle Module" -On Push,
 Pop-Location
 
 return 
-New-GitHubWorkflow -Name "Analyze, Test, Tag, and Publish" -On Push, PullRequest, Demand -Job PowerShellStaticAnalysis, TestPowerShellOnLinux, TagReleaseAndPublish, BuildModule |
-    Set-Content .\.github\workflows\TestAndPublish.yml -Encoding UTF8 -PassThru
 
